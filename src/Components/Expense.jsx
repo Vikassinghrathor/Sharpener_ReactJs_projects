276f01fc221ecea374a50ee43237e0e530f7b95e
vikassinghrathor <rathorevikas2809@gmail.com>
2023-11-10 11:36:17 +0530

    Expense Tracker 1 code

---
+++ src/Components/Expense.jsx
@@ -0,0 +1,12 @@
+function Expense(){
+  return(
+    <>
+    <h1>Expense Item</h1>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </>
+  )
+}
+
+export default Expense;
