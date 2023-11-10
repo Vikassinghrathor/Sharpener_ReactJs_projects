f2750191d61586589be23557beb8ae8594cc22ee
vikassinghrathor <rathorevikas2809@gmail.com>
2023-11-10 12:24:09 +0530

    make dynamic variable

--- src/Components/Expense.jsx
+++ src/Components/Expense.jsx
@@ -1,10 +1,12 @@
 function Expense(){
+  const LocationOfExpenditure = 'games';
   return(
     <>
     <h1>Expense Item</h1>
       <p>Food Rs 10</p>
       <p>Petrol Rs 100</p>
       <p>Movies Rs 200</p>
+      <p>{LocationOfExpenditure} Rs 300</p>
     </>
   )
 }
