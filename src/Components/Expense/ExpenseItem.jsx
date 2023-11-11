cc1b75c3b93ab8252d278cd35d38a9da05ec144e
vikassinghrathor <rathorevikas2809@gmail.com>
2023-11-11 15:24:14 +0530

    useState button amount

--- src/Components/Expense/ExpenseItem.jsx
+++ src/Components/Expense/ExpenseItem.jsx
@@ -1,23 +1,25 @@
+// ExpenseItem.js
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
-  
-  const DeleteExpanse = () => {
-    const expenseItemContainer = document.getElementById(`expense-item-${props.id}`);
-    
-    expenseItemContainer.parentNode.removeChild(expenseItemContainer);
+  const [changeInput, setChangeInput] = useState(props.amount);
 
-  }
+  const expenseHandler = () => {
+    setChangeInput('100$');
+    console.log(changeInput); // This will log the old value due to the asynchronous nature of state updates
+  };
+
   return (
-    <Card className='expense-item'>
+    <Card className='expense-item' id={`expense-item-${props.id}`}>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${changeInput}</div>
       </div>
-      <button onClick={DeleteExpanse}>Delete Expense</button>
+      <button onClick={expenseHandler}>Change Expense</button>
     </Card>
   );
 }
